cf24b821f95703e7ec8b5250c0a8dbb943cb9b74
Doreen Atambo <doreenatambo@Doreens-MacBook-Air.local>
2026-09-06 19:53:49 +0300

    fix allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='dev-secret-key-not-for-production')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['archives zetu-backend.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['archives-zetu-backend.onrender.com', 'localhost', '127.0.0.1']
 
 ARTICLE_UPLOAD_KEY = config('ARTICLE_UPLOAD_KEY')
 # Application definition
