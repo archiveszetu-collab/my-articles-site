153967a102b25d69f3d2361b9f5db373c0c8f78f
Doreen Atambo <doreenatambo@Doreens-MacBook-Air.local>
2026-09-06 18:43:44 +0300

    add gunicorn

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_wx9=hta-e7=oee*tu(=azs#he9qrt_yw4at9ky7s=s@%yt=*j'
+SECRET_KEY = config('SECRET_KEY', default='dev-secret-key-not-for-production') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-articles-backend.onrender.com', 'localhost', '127.0.0.1']
 
 ARTICLE_UPLOAD_KEY = config('ARTICLE_UPLOAD_KEY')
 # Application definition
@@ -78,16 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'articles_db',
-        'USER': 'doreenatambo',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql', 'NAME': config('DB_NAME'), 'USER': config('DB_USER'), 'PASSWORD': config('DB_PASSWORD'), 'HOST': config('DB_HOST'), 'PORT': config('DB_PORT'), } } 
 
 
 # Password validation
